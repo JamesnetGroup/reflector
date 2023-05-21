b44a8d29e396a33f8411c3ea821d0106967957e7
JAEWUNG LEE <james@jamesnet.dev>
2023-05-22 06:00:55 +0900

    Updates

--- src/Reflector.Assemblies/Local/ViewModels/AssemblyUnitViewModel.cs
+++ src/Reflector.Assemblies/Local/ViewModels/AssemblyUnitViewModel.cs
@@ -13,6 +13,8 @@ namespace Reflector.Assemblies.Local.ViewModels
     {
         [ObservableProperty]
         private TreeModel _currentAssembly;
+        [ObservableProperty]
+        public List<AssemInfo> _assemblyInfo;
 
         public List<TreeModel> Assemblies { get; init; }
 
@@ -52,6 +54,66 @@ namespace Reflector.Assemblies.Local.ViewModels
         private void AssemblyClick(TreeModel data)
         {
             CurrentAssembly = data;
+            if (data.Assem != null)
+            {
+                AssemblyInfo = GetAssemblyInfo(data.Assem);
+            }
         }
+
+        private List<AssemInfo> GetAssemblyInfo(Assembly assembly)
+        {
+            List<AssemInfo> assemblyInfoList = new List<AssemInfo>();
+
+            // List all the types (classes, interfaces, enums, etc.)
+            foreach (var type in assembly.GetTypes())
+            {
+                AssemInfo assemInfo = new AssemInfo
+                {
+                    TypeName = type.FullName,
+                    IsClass = type.IsClass,
+                    IsPublic = type.IsPublic,
+                    IsPrivate = !type.IsPublic && !type.IsNotPublic, // Non-public (internal types might also be included here)
+                    IsAbstract = type.IsAbstract,
+                    IsSealed = type.IsSealed,
+                    IsInterface = type.IsInterface
+                };
+
+                // List all the methods for each type
+                foreach (var method in type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static))
+                {
+                    assemInfo.Methods.Add(method.Name);
+                }
+
+                // List all the properties for each type
+                foreach (var prop in type.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static))
+                {
+                    assemInfo.Properties.Add(prop.Name);
+                }
+
+                // List all the fields for each type
+                foreach (var field in type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static))
+                {
+                    assemInfo.Fields.Add(field.Name);
+                }
+
+                assemblyInfoList.Add(assemInfo);
+            }
+
+            return assemblyInfoList;
+        }
+    }
+
+    public class AssemInfo
+    {
+        public string TypeName { get; set; }
+        public bool IsClass { get; set; }
+        public bool IsPublic { get; set; }
+        public bool IsPrivate { get; set; }
+        public bool IsAbstract { get; set; }
+        public bool IsSealed { get; set; }
+        public bool IsInterface { get; set; }
+        public List<string> Methods { get; set; } = new List<string>();
+        public List<string> Properties { get; set; } = new List<string>();
+        public List<string> Fields { get; set; } = new List<string>();
     }
 }
