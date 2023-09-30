81cbe971015f9b3e8eb2de1daa75ff09c876c2e3
JAEWUNG LEE <james@jamesnet.dev>
2023-09-30 16:47:54 +0900

    Updates

---
+++ src/Reflector.Data/Models/FieldDetails.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace Reflector.Data.Models
+{
+
+    public class FieldDetails
+    {
+        public FieldInfo Field { get; set; }
+        public string Name { get; set; }
+        public bool IsPublic { get; set; }
+        public bool IsPrivate { get; set; }
+        public bool IsStatic { get; set; }
+        public bool IsBackingField { get; set; }
+    }
+}
